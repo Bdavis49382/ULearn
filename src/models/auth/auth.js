1412cb79c2cae02e54a8c5f7b64a6e033178f2b6
Ben Davis <bendavis493@gmail.com>
2025-04-01 19:59:21 -0600

    switched to bcryptjs

--- src/models/auth/auth.js
+++ src/models/auth/auth.js
@@ -1,5 +1,5 @@
 import dbClient from "../index.js";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export async function register(code, firstName, lastName, email, password) {
     try {
